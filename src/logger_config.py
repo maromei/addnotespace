07b56fa25d4df6d5d10d66ec24382290193e7eee
maromei <max@pop-os.localdomain>
2023-04-30 12:23:54 +0200

    added basic logger config

---
+++ src/logger_config.py
@@ -0,0 +1,47 @@
+LOGGING_FORMATTERS = {
+    "standard": {
+        "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    },
+}
+
+LOGGING_HANDLERS = {
+    "console_info": {
+        "level": "INFO",
+        "formatter": "standard",
+        "class": "logging.StreamHandler",
+        "stream": "ext://sys.stdout"
+    },
+    "file_info": {
+        "level": "INFO",
+        "formatter": "standard",
+        "class": "logging.handlers.RotatingFileHandler",
+        "filename": "logs/log.txt",
+        "mode": "a+",
+        "maxBytes": 5 * 10**6, # 5 MegaBytes
+        "backupCount": 1
+    }
+}
+
+
+DEFAULT_LOGGER_CONFIG = {
+    "handlers": ["console_info", "file_info"],
+    "level": "WARNING",
+    "propagate": True
+}
+
+
+LOGGERS = {
+    "": DEFAULT_LOGGER_CONFIG,
+    "addnotespace": DEFAULT_LOGGER_CONFIG,
+    "addnotespace.settings": DEFAULT_LOGGER_CONFIG,
+    "addnotespace.style_loader": DEFAULT_LOGGER_CONFIG
+}
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": LOGGING_FORMATTERS,
+    "handlers": LOGGING_HANDLERS,
+    "loggers": LOGGERS
+}
