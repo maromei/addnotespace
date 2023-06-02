e472054bd5ba1f068df4ce5f6fb16d7e12a9ade6
maromei <max@pop-os.localdomain>
2023-06-02 13:41:29 +0200

    added simple cx_freeze build script

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_options = {"packages": [], "exludes": []}
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+executables = [
+    Executable("src/main.py", base=base),
+    Executable("src/run_bulk.py", base=base),
+]
+
+setup(
+    name="addnotespace",
+    version="1",
+    description="Add whitespace to the sides of your notes.",
+    options={"build_exe": build_options},
+    executables=executables,
+)
