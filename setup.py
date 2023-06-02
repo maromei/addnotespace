1478c5e52deb168484cd6a7c0e5c8371f594a21a
maromei <maxrob.meier@gmail.com>
2023-06-02 06:45:12 -0700

    Fixed build scripts

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from cx_Freeze import setup, Executable
 from src.__about__ import __version__
 
 version = __version__
+build_name = f"addnotespace_v{version}_x86"
 
-build_options = {"packages": [], "exludes": ["GitPython"]}
+build_options = {
+    "packages": [],
+    "excludes": ["GitPython"],
+    "build_exe": f"build/{build_name}",
+}
 
 base = "Win32GUI" if sys.platform == "win32" else None
 
 executables = [
     Executable("src/main.py", base=base),
-    Executable("src/run_bulk.py", base=base),
+    Executable("src/bulk_run.py", base=base),
 ]
 
 setup(
