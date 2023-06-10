3c48627ea44ef1053a94a5db928639e8911d5df4
maromei <max@pop-os.localdomain>
2023-06-10 19:15:29 +0200

    added dev versioning

--- scripts/write_version_to_file.py
+++ scripts/write_version_to_file.py
@@ -10,8 +10,16 @@ repo = git.Repo(PROJECT_ROOT)
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
 
 latest_tag = str(tags[-1])
+commit_count_since_tag = repo.git.rev_list("--count", f"{latest_tag}..HEAD")
+commit_count_since_tag = int(commit_count_since_tag)
+
 if latest_tag[0] == "v":
     latest_tag = latest_tag[1:]
 
+if commit_count_since_tag != 0:
+    latest_tag += f"-dev{commit_count_since_tag+1}"
+
 with open(VERSION_FILE, "w") as f:
     f.write(f'__version__ = "{latest_tag}"\n')
+
+repo.index.add([VERSION_FILE])
